1c88971004933e3feff4aebe80f52edd2810d643
antony-kimanzi <antonykimanzi29@gmail.com>
2026-04-29 01:29:29 +0300

    add new project in project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
-import "./Projects.css"
+import React from "react";
+import "./Projects.css";
 
 export default function Projects() {
-
   return (
-    <div className='container'>
-      <a href='https://bluepacknova.com/' target='_blank'><div className='project-section'>
-        <div className='bg-image-style'></div>
-        <div className='text-section'><div className='project-hdr'><h5>Bluepack Nova</h5><img src="images/link-arrow.svg" alt="resume-arrow-image" className="h-[24px] w-[24px] mt-[10px] ml-[10px]" /></div><p>An ecommerce web app that offers a streamline online platform for businesses and individuals to easily order high-quality IoT and robotics products.</p></div>
-      </div></a>
+    <div className="container">
+      <a href="https://bluepacknova.com/" target="_blank">
+        <div className="project-section">
+          <div className="bg-image-style-1"></div>
+          <div className="text-section">
+            <div className="project-hdr">
+              <h5>Bluepack Nova</h5>
+              <img
+                src="images/link-arrow.svg"
+                alt="resume-arrow-image"
+                className="h-[24px] w-[24px] mt-[10px] ml-[10px]"
+              />
+            </div>
+            <p>
+              An ecommerce web app that offers a streamline online platform for
+              businesses and individuals to easily order high-quality IoT and
+              robotics products.
+            </p>
+          </div>
+        </div>
+      </a>
+      <a href="https://scentgalore.vercel.app/" target="_blank">
+        <div className="project-section">
+          <div className="bg-image-style-2"></div>
+          <div className="text-section">
+            <div className="project-hdr">
+              <h5>ScentGalore</h5>
+              <img
+                src="images/link-arrow.svg"
+                alt="resume-arrow-image"
+                className="h-[24px] w-[24px] mt-[10px] ml-[10px]"
+              />
+            </div>
+            <p>
+              A streamlined ecommerce platform for individuals and businesses to
+              easily discover and order high-quality perfumes—from designer
+              classics to niche fragrances—with a fast, secure checkout.
+            </p>
+          </div>
+        </div>
+      </a>
     </div>
-  )
+  );
 }
